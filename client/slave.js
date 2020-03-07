ee5827f25fef3fd91c83305c4619d5a2fe23316f
Dariush <dariush007@gmx.ch>
2020-03-07 21:09:03 +0100

    on click

--- client/slave.js
+++ client/slave.js
@@ -31,11 +31,11 @@ let slaveSketch = function (p) {
 
     p.draw = function () {
 
-        p.stroke(0)
+     /*     p.stroke(0)
         p.background(255);
         // let spectrum = p.fft.analyze();
         //console.log(spectrum)
-        //at what index of energies is the max?
+        //at what index of energies is the max?*/
         let indexOfMaxValue = indexOfMax(spectrum);
         let peakFreq = Math.round(indexOfMaxValue * (p.sampleRate() / 2) / spectrum.length)
         if (peakFreq != undefined && peakFreq > 0) {
@@ -60,12 +60,12 @@ let slaveSketch = function (p) {
 
 
         }
-        p.beginShape();
+        /*  p.beginShape();
         for (i = 0; i < spectrum.length; i++) {
             p.vertex(i, p.map(spectrum[i], 0, 255, p.height, 0));
         }
         p.endShape();
-       /* if (spectrum[indexOfMaxValue] > p.peakMinAmp) {
+      if (spectrum[indexOfMaxValue] > p.peakMinAmp) {
             p.fill(50)
             p.text("Amp: " + spectrum[indexOfMaxValue], indexOfMaxValue, p.height / 2)
             if (frequencyFound == peakFreq) { p.fill(10, 255, 10); p.stroke(10, 255, 10) }
@@ -97,103 +97,4 @@ let slaveSketch = function (p) {
     }
 }
 
-var webaudio_tooling_obj = function () {
 
-    var audioContext = new AudioContext();
-
-    console.log("audio is starting up ...");
-
-    var BUFF_SIZE = 16384;
-
-    var audioInput = null,
-        microphone_stream = null,
-        gain_node = null,
-        script_processor_node = null,
-        script_processor_fft_node = null,
-        analyserNode = null;
-
-    if (!navigator.getUserMedia)
-        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-            navigator.mozGetUserMedia || navigator.msGetUserMedia;
-
-    if (navigator.getUserMedia) {
-
-        navigator.getUserMedia({ audio: true },
-            function (stream) {
-                start_microphone(stream);
-            },
-            function (e) {
-                alert('Error capturing audio.');
-            }
-        );
-
-    } else { alert('getUserMedia not supported in this browser.'); }
-
-    // ---
-
-    function show_some_data(given_typed_array, num_row_to_display, label) {
-
-        var size_buffer = given_typed_array.length;
-        var index = 0;
-        var max_index = num_row_to_display;
-
-        // console.log("__________ " + label);
-
-        for (; index < max_index && index < size_buffer; index += 1) {
-            console.log(given_typed_array[index]);
-        }
-    }
-
-    function process_microphone_buffer(event) { // invoked by event loop
-
-        var i, N, inp, microphone_output_buffer;
-
-        microphone_output_buffer = event.inputBuffer.getChannelData(0); // just mono - 1 channel for now
-
-        // microphone_output_buffer  <-- this buffer contains current gulp of data size BUFF_SIZE
-
-        show_some_data(microphone_output_buffer, 5, "from getChannelData");
-    }
-
-    function start_microphone(stream) {
-
-        gain_node = audioContext.createGain();
-        gain_node.connect(audioContext.destination);
-
-        microphone_stream = audioContext.createMediaStreamSource(stream);
-
-        script_processor_node = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
-        script_processor_node.onaudioprocess = process_microphone_buffer;
-
-        microphone_stream.connect(script_processor_node);
-
-
-        // --- setup FFT
-
-        script_processor_fft_node = audioContext.createScriptProcessor(1024, 1, 1);
-        script_processor_fft_node.connect(gain_node);
-
-        analyserNode = audioContext.createAnalyser();
-        analyserNode.smoothingTimeConstant = 0.9;
-        analyserNode.minDecibels = -80;
-        analyserNode.fftSize = BUFF_SIZE;
-
-        microphone_stream.connect(analyserNode);
-
-        analyserNode.connect(script_processor_fft_node);
-
-        script_processor_fft_node.onaudioprocess = function () {
-            analyserNode.smoothingTimeConstant = 0.8
-            // get the average for the first channel
-            // let spectrum = new Uint8Array(analyserNode.frequencyBinCount);
-            analyserNode.getByteFrequencyData(spectrum);
-            // console.log( analyserNode.frequencyBinCount)
-            // draw the spectrogram
-            /* if (microphone_stream.playbackState == microphone_stream.PLAYING_STATE) {
-     
-                 show_some_data(spectrum, 5, "from fft");
-             }*/
-        };
-    }
-
-}(); //  webaudio_tooling_obj = function()
