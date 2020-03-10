7d4e5576e7e6bea0214bc22c935a721dad678dcd
Dariush <dariush007@gmx.ch>
2020-03-10 15:36:09 +0100

    set freq back to 200 n refactoring

--- client/slave.js
+++ client/slave.js
@@ -119,4 +119,98 @@ let slaveSketch = function (p) {
     }
 }
 
+
+
+function startMicrophoneInput() {
+
+    var webaudio_tooling_obj = function () {
+
+        var audioContext = new AudioContext();
+
+        console.log("🎙️ Mic is starting up ...");
+
+        var BUFF_SIZE = 16384;
+
+        var audioInput = null,
+            microphone_stream = null,
+            gain_node = null,
+            script_processor_node = null,
+            script_processor_fft_node = null,
+            analyserNode = null;
+
+
+        navigator.getMic = (navigator.getUserMedia || navigator.webKitGetUserMedia || navigator.moxGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+        if (navigator.mediaDevices.getUserMedia) {
+            navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+                .then(function (stream) {
+                    start_microphone(stream);    //Display the video stream in the video object
+                })
+                .catch(function (e) { logError(e.name + ": " + e.message); });
+        }
+        else {
+            navigator.getMic({ audio: true, video: false },
+                function (stream) {
+                    start_microphone(stream);
+                },
+                function (e) { console.log("⛔ Microphone  is not accessible." + e); });
+        }
+
+        // ---
+
+
+        function process_microphone_buffer(event) { // invoked by event loop
+
+            var i, N, inp, microphone_output_buffer;
+
+            microphone_output_buffer = event.inputBuffer.getChannelData(0); // just mono - 1 channel for now
+
+            // microphone_output_buffer  <-- this buffer contains current gulp of data size BUFF_SIZE
+
+        }
+
+        function start_microphone(stream) {
+
+            gain_node = audioContext.createGain();
+            gain_node.connect(audioContext.destination);
+
+            microphone_stream = audioContext.createMediaStreamSource(stream);
+
+            script_processor_node = audioContext.createScriptProcessor(BUFF_SIZE, 1, 1);
+            script_processor_node.onaudioprocess = process_microphone_buffer;
+
+            microphone_stream.connect(script_processor_node);
+
+
+            // --- setup FFT
+
+            script_processor_fft_node = audioContext.createScriptProcessor(1024, 1, 1);
+            script_processor_fft_node.connect(gain_node);
+
+            analyserNode = audioContext.createAnalyser();
+            analyserNode.smoothingTimeConstant = 0.7;
+            // analyserNode.minDecibels = -80;
+            analyserNode.fftSize = BUFF_SIZE;
+
+            microphone_stream.connect(analyserNode);
+
+            analyserNode.connect(script_processor_fft_node);
+
+            script_processor_fft_node.onaudioprocess = function () {
+                analyserNode.smoothingTimeConstant = 0.8
+                // let spectrum = new Uint8Array(analyserNode.frequencyBinCount);
+                analyserNode.getByteFrequencyData(spectrum);
+                // console.log( analyserNode.frequencyBinCount)
+                // draw the spectrogram
+                /* if (microphone_stream.playbackState == microphone_stream.PLAYING_STATE) {
+         
+                     show_some_data(spectrum, 5, "from fft");
+                 }*/
+            };
+        }
+
+    }();//  webaudio_tooling_obj = function()
+
+
+}
+
 let player = new Tone.Player("./audio/Metal_Hit3.WAV").toMaster();
